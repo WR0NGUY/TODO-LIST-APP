cf180d947545d1e1bcaaec53316b7fafb5804f73
Amit Kumar <itswronguy@gmail.com>
2025-04-05 19:53:38 +0530

    Hook: useRef and functional State Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,52 @@
-import AppName from "./components/AppName"
-import AddTodo from "./components/AddTodo"
-import TodoItems from "./components/TodoItems"
-import "./App.css"
-import Message from "./components/Message"
-import { useState } from "react"
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import "./App.css";
+import Message from "./components/Message";
+import { useState } from "react";
 
 function App() {
-
-  const initialTodoItems = [{
-    name: 'Exercise',
-    dueDate: '24/3/25'
-  },
-  {
-    name: 'Study',
-    dueDate: '25/3/25'
-  },
-  {
-    name: 'Sleep',
-    dueDate: '26/3/25'
-  },
+  const initialTodoItems = [
+    {
+      name: "Exercise",
+      dueDate: "24/3/25",
+    },
+    {
+      name: "Study",
+      dueDate: "25/3/25",
+    },
+    {
+      name: "Sleep",
+      dueDate: "26/3/25",
+    },
   ];
 
   const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
-    console.log(`New item Added Name:${itemName} Date:${itemDueDate}`)
-    const newTodoItems = [...todoItems, {
-      name: itemName,
-      dueDate: itemDueDate
-    }]
-
-    setTodoItems(newTodoItems)
-  }
+    setTodoItems((currValue) => [
+      ...currValue,
+      {
+        name: itemName,
+        dueDate: itemDueDate,
+      },
+    ]);
+  };
 
   const handleDeleteItems = (todoItemName) => {
-    const newTodoItems = todoItems.filter(item => item.name!== todoItemName)
-    setTodoItems(newTodoItems)
-  }
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
 
   return (
     <center className="todo-container main-container">
       <AppName />
       <AddTodo onNewItem={handleNewItem} />
       {todoItems.length === 0 && <Message></Message>}
-      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItems}></TodoItems>
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItems}
+      ></TodoItems>
     </center>
   );
 }
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { MdAssignmentAdd } from "react-icons/md";
 
 function AddTodo({ onNewItem }) {
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-    const[todoName, setTodoName] = useState("")
-    const[dueDate, setDueDate] = useState("")
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+    onNewItem(todoName, dueDate);
+  };
 
-    const handleNameChange = (event) => {
-        setTodoName(event.target.value)
-    }
-    const handleDateChange = (event) => {
-        setDueDate(event.target.value)
-    }
-
-    const handleAddButtonClicked = () => {
-        onNewItem(todoName, dueDate)
-        setDueDate("")
-        setTodoName("")
-    }
-
-    return (
-        <div className="container items-container">
-            <div className="row new-row">
-                <div className="col-5"><input type="text" placeholder="Enter to-do here" value={todoName} onChange={handleNameChange}/></div>
-                <div className="col-3"><input type="date" value={dueDate} onChange={handleDateChange}/></div>
-                <div className="col-2"><button type="button" className="btn btn-success new-btn" onClick={handleAddButtonClicked}> <MdAssignmentAdd /></button></div>
-            </div>
+  return (
+    <div className="container items-container">
+      <form className="row new-row" onSubmit={handleAddButtonClicked}>
+        <div className="col-5">
+          <input
+            type="text"
+            ref={todoNameElement}
+            placeholder="Enter to-do here"
+          />
         </div>
-    );
+        <div className="col-3">
+          <input type="date" ref={dueDateElement} />
+        </div>
+        <div className="col-2">
+          <button type="submit" className="btn btn-success new-btn">
+            {" "}
+            <MdAssignmentAdd />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default AddTodo;
